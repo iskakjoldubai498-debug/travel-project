c9f6bec13d3a43075cf81c9d2b32280c72ea71e6
iskak235 <iskakzoldubaj5@gmail.com>
2026-03-15 04:02:17 +0600

    Fix project name to mysite and storage settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -2,12 +2,6 @@ import os
 import dj_database_url
 from pathlib import Path
 
-# Пакеттерди алдын ала текшерүү (Python 3.13 үчүн)
-try:
-    import psycopg
-except ImportError:
-    pass
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2z*&rg!%vl03^jh=ns6-y1k$u)=$3rgdx%fmh#a2%wjhr)z7j4'
 DEBUG = True
@@ -25,7 +19,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'whitenoise.runserver_nostatic',
+    'whitenoise.runserver_nostatic', # Статика үчүн WhiteNoise
     'django.contrib.staticfiles',
     'cloudinary',
     'main',
@@ -33,7 +27,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', # Бул сап сөзсүз керек
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,16 +49,14 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
+                'django.contrib.auth.auth_context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.i18n',
             ],
         },
     },
 ]
 
-# --- БАЗА ЖӨНДӨӨСҮ ---
-# Render'де болсо PostgreSQL, компьютерде болсо SQLite иштетмей кылдык
+# Базаны локалдык жана сервердик версияга ылайыктоо
 DATABASES = {
     'default': dj_database_url.config(
         default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}",
@@ -79,6 +71,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Эгер сиздин 'static' деген папкаңыз жок болсо, бул ката бербеши үчүн:
+if not os.path.exists(os.path.join(BASE_DIR, 'static')):
+    os.makedirs(os.path.join(BASE_DIR, 'static'))
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
@@ -92,6 +89,7 @@ CLOUDINARY_STORAGE = {
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# МААНИЛҮҮ: Бул жерди гана өзгөрттүк (STATICFILES_STORAGE деген сапты таптакыр кошпоңуз!)
 STORAGES = {
     "default": {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
